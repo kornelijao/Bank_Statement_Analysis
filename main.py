c91256fb1c91f350fd4efad393bb99c5f759963e
kornelijao <kornelijaorlova@gmail.com>
2024-04-26 12:08:08 +0300

    Fixed and optimized some parts of the code.

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ import csv
 print()
 
 file_path = 'sampleData.csv'
-with open(file_path, 'r', encoding='utf-8') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    headers = next(csv_reader)
-    print(headers)
-    for row in csv_reader:
-        print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+def read_csv(file_path):
+    data = []
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        headers = next(csv_reader)
+        print(headers)
+        for row in csv_reader:
+            data.append(row)
+    return data
 
 #  1) kokios valiutos buvo naudotos?
 #  2) kiek income, outcome?(ignoruojant valiutas)
 print()
+def process_csv(file_path):
+    currencies_used = set()
+    total_income = 0
+    total_outcome = 0
 
-currencies_used = set()
-total_income = 0
-total_outcome = 0
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
 
-with open(file_path, 'r', encoding='utf-8') as csvfile:
-    csv_reader = csv.DictReader(csvfile)
-
-    for row in csv_reader:
-        currency = row['Valiuta']
-        currencies_used.add(currency)
+        for row in csv_reader:
+            currency = row['Valiuta']
+            currencies_used.add(currency)
 
         # income = float(row['Suma'])
         # if row['D/K'] == 'K':
@@ -36,143 +39,182 @@ with open(file_path, 'r', encoding='utf-8') as csvfile:
         # if row['D/K'] == 'D':
         #     total_outcome += outcome
 
-        amount = float(row['Suma'])
-        if row['D/K'] == 'K':
-            total_income += amount
-        elif row['D/K'] == 'D':
-            total_outcome += amount
+            amount = float(row['Suma'])
+            if row['D/K'] == 'K':
+                total_income += amount
+            elif row['D/K'] == 'D':
+                total_outcome += amount
+    return currencies_used, total_income, total_outcome
 
-print("Part 1")
-print()
-print("These currencies were used:", currencies_used)
-print()
-print("Part 2")
-print("Total income (currencies are ignored):", total_income)
-print("Total outcome (currencies are ignored):", total_outcome)
+def print_results(currencies_used, total_income, total_outcome):
+    print("Part 1")
+    print()
+    print("These currencies were used:", currencies_used)
+    print()
+    print("Part 2")
+    print("Total income (currencies are ignored):", total_income)
+    print("Total outcome (currencies are ignored):", total_outcome)
+
+file_path = 'sampleData.csv'
+currencies_used, total_income, total_outcome = process_csv(file_path)
+print_results(currencies_used, total_income, total_outcome)
 
 #  3) Kiek income, outcome pagal kiekvieną valiutą?
 
-income_by_currency = {}
-outcome_by_currency = {}
+def cal_income_outcome_by_currency(file_path):
+    income_by_currency = {}
+    outcome_by_currency = {}
 
-with open(file_path, 'r', encoding='utf-8') as csvfile:
-    csv_reader = csv.DictReader(csvfile)
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
 
-    for row in csv_reader:
-        currency = row['Valiuta']
-        amount = float(row['Suma'])
+        for row in csv_reader:
+            currency = row['Valiuta']
+            amount = float(row['Suma'])
 
-        if row['D/K'] == 'K':
-            if currency in income_by_currency:
-                income_by_currency[currency] += amount
-            elif income_by_currency == 0:
-                print("income_by_currency[currency] = 'No income'")
-            else:
-                income_by_currency[currency] = amount
-        elif row['D/K'] == 'D':
-            if currency in outcome_by_currency:
-                outcome_by_currency[currency] += amount
-            else:
-                outcome_by_currency[currency] = amount
+            if row['D/K'] == 'K':
+                if currency in income_by_currency:
+                    income_by_currency[currency] += amount
+                elif income_by_currency == 0:
+                    print("income_by_currency[currency] = 'No income'")
+                else:
+                    income_by_currency[currency] = amount
+            elif row['D/K'] == 'D':
+                if currency in outcome_by_currency:
+                    outcome_by_currency[currency] += amount
+                else:
+                    outcome_by_currency[currency] = amount
+    return income_by_currency, outcome_by_currency
 
-print()
-print("Part 3")
-print("Total income by currency:", )
-for currency, total_income in income_by_currency.items():
-    print(f"{currency}: {total_income}")
+def print_income_outcome_by_currency(income_by_currency, outcome_by_currency):
+    print()
+    print("Part 3")
+    print("Total income by currency:", )
+    for currency, total_income in income_by_currency.items():
+        print(f"{currency}: {total_income}")
 
-print("\nTotal outcome by currency:")
-for currency, total_outcome in outcome_by_currency.items():
-    print(f"{currency}: {total_outcome}")
+    print("\nTotal outcome by currency:")
+    for currency, total_outcome in outcome_by_currency.items():
+        print(f"{currency}: {total_outcome}")
+
+file_path = 'sampleData.csv'
+income, outcome = cal_income_outcome_by_currency(file_path)
+print_income_outcome_by_currency(income, outcome)
 
 # 4) kiek buvo išleista kiekvieną mėnesį?
 print()
 print("Part 4")
 
-outcome_by_month = {}
-with open(file_path, 'r', encoding='utf-8') as csvfile:
-    csv_reader = csv.DictReader(csvfile)
+def cal_expenses_by_month(file_path):
+    outcome_by_month = {}
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
 
-    for row in csv_reader:
-        date = row['Data']
-        amount = float(row['Suma'])
-        month = date.split('-')[1]
+        for row in csv_reader:
+            date = row['Data']
+            amount = float(row['Suma'])
+            month = date.split('-')[1]
 
-        if row['D/K'] == 'D':
-            if month in outcome_by_month:
-                outcome_by_month[month] += amount
-            else:
-                outcome_by_month[month] = amount
+            if row['D/K'] == 'D':
+                if month in outcome_by_month:
+                    outcome_by_month[month] += amount
+                else:
+                    outcome_by_month[month] = amount
+    return outcome_by_month
 
-print(f"Expenses by month: {outcome_by_month}")
+def print_expenses_by_month(outcome_by_month):
+    print("Part 4")
+    print(f"Expenses by month: {outcome_by_month}")
+    for month, total_outcome in outcome_by_month.items():
+        print(f"{month}: {total_outcome}")
+
+file_path = 'sampleData.csv'
+expenses = cal_expenses_by_month(file_path)
+print_expenses_by_month(expenses)
 
 # 5) kiek buvo uždirbta kiekvieną mėnesį?
 print()
 print("Part 5")
 
-income_by_month = {}
-with open(file_path, 'r', encoding='utf-8') as csvfile:
-    csv_reader = csv.DictReader(csvfile)
+def cal_income_by_month(file_path):
+    income_by_month = {}
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
 
-    for row in csv_reader:
-        date = row['Data']
-        amount = float(row['Suma'])
-        month = date.split('-')[1]
+        for row in csv_reader:
+            date = row['Data']
+            amount = float(row['Suma'])
+            month = date.split('-')[1]
 
-        if row['D/K'] == 'K':
-            if month in income_by_month:
-                income_by_month[month] += amount
-            else:
-                income_by_month[month] = amount
+            if row['D/K'] == 'K':
+                if month in income_by_month:
+                    income_by_month[month] += amount
+                else:
+                    income_by_month[month] = amount
+    return income_by_month
 
-print(f"Income by month: {income_by_month}")
+def print_income_by_month(income_by_month):
+    print("Part 5")
+    print(f"Income by month: {income_by_month}")
+    for month, total_income in income_by_month.items():
+        print(f"{month}: {total_income}")
+
+file_path = 'sampleData.csv'
+income = cal_income_by_month(file_path)
+print_income_by_month(income)
 
 # 6) koks pinigų likutis kiekvieno mėnesio gale? (sausio pradžioje likutis buvo 0.00)(ignoruojant valiutas).
 print()
 print("Part 6")
 
-income_in_end_month = {}
-outcome_in_end_month = {}
-balance_in_end_month = {}
-balance = 0.00
+def cal_balance_by_month(file_path):
+    income_in_end_month = {}
+    outcome_in_end_month = {}
+    balance_in_end_month = {}
+    balance = 0.00
 
-with open(file_path, 'r', encoding='utf-8') as csvfile:
-    csv_reader = csv.DictReader(csvfile)
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
 
-    for row in csv_reader:
-        date = row['Data']
-        amount = float(row['Suma'])
-        month = date.split('-')[1]
+        for row in csv_reader:
+            date = row['Data']
+            amount = float(row['Suma'])
+            month = date.split('-')[1]
 
-        if row['D/K'] == 'K':
-            if month in income_in_end_month:
-                income_in_end_month[month] += amount
-            else:
-                income_in_end_month[month] = amount
-        elif row['D/K'] == 'D':
-            if month in outcome_in_end_month:
-                outcome_in_end_month[month] += amount
-            else:
-                outcome_in_end_month[month] = amount
+            if row['D/K'] == 'K':
+                if month in income_in_end_month:
+                    income_in_end_month[month] += amount
+                else:
+                    income_in_end_month[month] = amount
+            elif row['D/K'] == 'D':
+                if month in outcome_in_end_month:
+                    outcome_in_end_month[month] += amount
+                else:
+                    outcome_in_end_month[month] = amount
 
-for month in sorted(set(list(income_in_end_month.keys()) + list(outcome_in_end_month.keys()))):
+    for month in sorted(set(list(income_in_end_month.keys()) + list(outcome_in_end_month.keys()))):
+        total_income = income_in_end_month.get(month, 0)
+        total_outcome = outcome_in_end_month.get(month, 0)
+        balance += total_income - total_outcome
+        balance_in_end_month[month] = balance
 
-    total_income = income_in_end_month.get(month, 0)
-    total_outcome = outcome_in_end_month.get(month, 0)
+    return balance_in_end_month
 
-    balance += total_income - total_outcome
-    balance_in_end_month[month] = balance
+def print_balance_by_month(balance_by_month):
+    print(f"Balance in the end of each month: {balance_by_month}")
+    for month, balance in balance_by_month.items():
+        print(f"Month {month}: {balance}")
 
-print("Balance in the end of each month:")
-for month, balance in balance_in_end_month.items():
-    print(f"Month {month}: {balance}")
+file_path = 'sampleData.csv'
+balances = cal_balance_by_month(file_path)
+print_balance_by_month(balances)
 
 # 7) Koks pinigų likutis kiekvieno mėnesio gale? (sausio pradžioje likutis buvo 0.00) pagal kiekvieną valiutą?
 print()
 print("Part 7")
 
 def calc_monthly_balance_by_currency(file_path):
-    balance_by_month_and_currency = {}
+    bal_by_month_and_currency = {}
 
     with open(file_path, 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.DictReader(csvfile)
@@ -183,23 +225,57 @@ def calc_monthly_balance_by_currency(file_path):
             month = date.split('-')[1]
             currency = row['Valiuta']
 
-            if month not in balance_by_month_and_currency:
-                balance_by_month_and_currency[month] = {}
-            if currency not in balance_by_month_and_currency[month]:
-                balance_by_month_and_currency[month][currency] = 0.0
+            if month not in bal_by_month_and_currency:
+                bal_by_month_and_currency[month] = {}
+            if currency not in bal_by_month_and_currency[month]:
+                bal_by_month_and_currency[month][currency] = 0.0
             if row['D/K'] == 'K':
-                balance_by_month_and_currency[month][currency] += amount
+                bal_by_month_and_currency[month][currency] += amount
             elif row['D/K'] == 'D':
-                balance_by_month_and_currency[month][currency] -= amount
+                bal_by_month_and_currency[month][currency] -= amount
 
-    return balance_by_month_and_currency
+    return bal_by_month_and_currency
 
+def print_monthly_balance(bal_by_month_and_currency):
+# result = calc_monthly_balance_by_currency(file_path)
+    for month, balances in bal_by_month_and_currency.items():
+        print(f"Month {month}:")
+        for currency, balance in balances.items():
+            print(f"Currency {currency}: {balance}")
+
+file_path = 'sampleData.csv'
 result = calc_monthly_balance_by_currency(file_path)
-for month, balances in result.items():
-    print(f"Month {month}:")
-    for currency, balance in balances.items():
-        print(f"Currency {currency}: {balance}")
+print_monthly_balance(result)
 
 # 7) Atvaizduokite per procentinę išraišką pamėnesiui pajamas ir išlaidas procentine išraiška.
+
+def print_percentage_income_outcome_by_month(file_path):
+    income_by_month = cal_income_by_month(file_path)
+    expenses_by_month = cal_expenses_by_month(file_path)
+    total_income = sum(income_by_month.values())
+    total_expenses = sum(expenses_by_month.values())
+
+    percent_inc_out_month = {}
+
+    for month, income in income_by_month.items():
+        expense = expenses_by_month.get(month, 0)
+        if total_income > 0:
+            income_percent = (income / total_income) * 100
+        else:
+            income_percent = 0
+        if total_expenses > 0:
+            expenense_percent = (expense / total_expenses)
+        else:
+            outcome_percent = 0
+
+        percent_inc_out_month[month] = {'Income': income_percent, 'Expenses': expenense_percent}
+
+    print("Percentage value of income and outcome by month:")
+    for month, percentages in percent_inc_out_month.items():
+        print(f"Month {month}: Income: {percentages['Income']:.2f}%, Expenses: {percentages['Expenses']:.2f}%")
+
+file_path = 'sampleData.csv'
+print_percentage_income_outcome_by_month(file_path)
+
 
 
